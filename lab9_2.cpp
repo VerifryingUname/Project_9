cbacd562fec087082dfac6a12c581340c832b630
VerifryingUname <kingdagone1@gmail.com>
2024-12-16 15:03:22 +0700

    lab9_2.cpp completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,24 +1,13 @@
-#include<iostream>
-using namespace std;
+void printO(int row, int count) {
+    if (row <= 0 or count <= 0) {
+        cout << "Invalid input" << endl;
+        return;
+    }
 
-//Write the function printO() here
-
-int main(){
-	
-	printO(2,2);
-	cout << "\n";
-	
-	printO(3,5);
-	cout << "\n";	
-	
-	printO(5,3);
-	cout << "\n";	
-	
-	printO(0,3);
-	cout << "\n";	
-	
-	printO(7,-1);
-	cout << "\n";	
-	
-	return 0;
+    for (int i = 0; i < row; i++) { 
+        for (int j = 0; j < count; j++) { 
+            cout << "O"; 
+        }
+        cout << endl; 
+    }
 }
