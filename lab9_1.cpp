466a406fa165fa7a802c12129389037c8bedb3a1
VerifryingUname <kingdagone1@gmail.com>
2024-12-16 15:02:39 +0700

    lab9_1.cpp completed

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,32 +2,42 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+    double lona, IRPPY,PPY;
 	cout << "Enter initial loan: ";
+	cin >> lona;
 	cout << "Enter interest rate per year (%): ";
+	cin >> IRPPY;
 	cout << "Enter amount you can pay per year: ";
-
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
+	cin >> PPY;
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
-	cout << setw(13) << left << "Total";
-	cout << setw(13) << left << "Payment";
-	cout << setw(13) << left << "NewBalance";
-	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
+    cout << setw(13) << left << "Total";
+    cout << setw(13) << left << "Payment";
+    cout << setw(13) << left << "NewBalance";
+    cout << "\n";
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	int year = 1;
+	while(lona > 0){
+	double charge = lona * (IRPPY / 100);
+	double total = lona + charge;
+	double paid;
+	if(total < PPY){
+	    paid = total;
+	}else{
+	    paid = PPY;
+	}
+	double balance = total - paid;
+    cout << setw(13) << left << year;
+    cout << setw(13) << left << lona;
+    cout << setw(13) << left << charge;
+    cout << setw(13) << left << total ;
+    cout << setw(13) << left << paid;
+    cout << setw(13) << left << balance;
+    cout << "\n";	
+    lona = balance;
+    year++;
+	}
 	return 0;
 }
